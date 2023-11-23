97a222d1e63b7122b5802aca09bfac53a495f945
hadhi hassan m <diluhadhi01@gmail.com>
2023-11-22 16:22:00 -0800

    todolist working completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import Counter from './Counter';
 import './app.css';
 import { useState } from 'react';
 
 function App() {
- 
-   return (
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDO] = useState('');
+
+  return (
     <div className="app">
       <div className="mainHeading">
         <h1>ToDo List</h1>
@@ -14,19 +15,52 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e) => setToDO(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false },])} className="fas fa-plus" ></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+        {toDos.map((value, index) => (
+          <div className="todo" key={value.id}>
+            <div className="left">
+              <input
+                onChange={(e) => {
+                  setToDos((prevToDos) =>
+                    prevToDos.map((obj) =>
+                      obj.id === value.id ? { ...obj, status: e.target.checked } : obj
+                    )
+                  );
+                }}
+                value={value.status}
+                type="checkbox"
+                name=""
+                id=""
+              />
+              <p>{value.text}</p>
+            </div>
+            <div className="right">
+
+              <i onClick={() => {
+                setToDos((prevToDos) => {
+                  console.log(prevToDos,index);
+                  const newToDos = [...prevToDos];
+                  newToDos.splice(index, 1);
+                  return newToDos;
+                });
+              }} className="fas fa-times"></i>
+            </div>
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        ))}
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return(
+              <div> 
+              <h3 style={{ color: 'white' }} key={obj.id}>{obj.text}</h3>
+              </div>
+
+            )
+          }
+          return null;
+        })}
       </div>
     </div>
   );
